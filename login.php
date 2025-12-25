0191a0f85809d323d49d61847bc7b59654decd2c
Ugryzh <34131622+ugryzh@users.noreply.github.com>
2025-12-25 19:05:45 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,6 @@
+<form method="post" action="api/login.php">
+  <h2>Logowanie</h2>
+  <input name="username" placeholder="Nazwa użytkownika" required>
+  <input name="password" type="password" placeholder="Hasło" required>
+  <button>Zaloguj</button>
+</form>
