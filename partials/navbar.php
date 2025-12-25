2a40f7b295838ea956824e4f1e89004d03696033
Ugryzh <34131622+ugryzh@users.noreply.github.com>
2025-12-25 20:58:50 +0100

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,17 +1,21 @@
 <?php
 require_once __DIR__ . '/../api/auth.php';
+$user = isLogged() ? currentUser() : null;
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-glass fixed-top">
+<nav class="navbar navbar-expand-lg bg-glass fixed-top">
   <div class="container">
-    <a class="navbar-brand" href="/index.php">Hangul Learn</a>
+
+    <a class="navbar-brand fw-bold" href="/index.php">
+      Hangul Learn
+    </a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMain">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navMain">
-      <ul class="navbar-nav ms-auto">
+      <ul class="navbar-nav ms-auto align-items-lg-center">
 
         <li class="nav-item">
           <a class="nav-link" href="/ranking.php">Ranking</a>
@@ -21,36 +25,50 @@ require_once __DIR__ . '/../api/auth.php';
           <a class="nav-link" href="/mikroblog.php">Mikroblog</a>
         </li>
 
-        <?php if (isLogged()): ?>
+        <?php if ($user): ?>
+
           <li class="nav-item">
             <a class="nav-link" href="/dashboard.php">Dashboard</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="/u/<?= urlencode(currentUser()['username']) ?>">
+            <a class="nav-link" href="/u/<?= urlencode($user['username']) ?>">
               Profil
             </a>
           </li>
 
           <?php if (isAdmin()): ?>
             <li class="nav-item">
-              <a class="nav-link text-warning" href="/admin/index.php">Admin</a>
+              <a class="nav-link text-warning" href="/admin/index.php">
+                Admin
+              </a>
             </li>
           <?php endif; ?>
 
-          <li class="nav-item">
-            <a class="nav-link" href="/logout.php">Wyloguj</a>
+          <li class="nav-item ms-lg-2">
+            <a class="btn btn-outline-danger btn-sm" href="/logout.php">
+              Wyloguj
+            </a>
           </li>
+
         <?php else: ?>
+
           <li class="nav-item">
-            <a class="nav-link" href="/login.php">Logowanie</a>
+            <a class="nav-link" href="#" onclick="openAuth('login')">
+              Logowanie
+            </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/register.php">Rejestracja</a>
+
+          <li class="nav-item ms-lg-2">
+            <a class="btn btn-primary btn-sm" href="#" onclick="openAuth('register')">
+              Rejestracja
+            </a>
           </li>
+
         <?php endif; ?>
 
       </ul>
     </div>
+
   </div>
 </nav>
