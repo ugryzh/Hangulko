bdcbc590899e74281ee39a28d339b08e228797cd
Ugryzh <34131622+ugryzh@users.noreply.github.com>
2025-12-25 19:09:47 +0100

    Update auth.php

--- api/auth.php
+++ api/auth.php
@@ -1,14 +1,16 @@
 <?php
 require_once 'db.php';
 
-function isLogged() {
+function isLogged(): bool {
     return isset($_SESSION['user_id']);
 }
 
-function isAdmin() {
+function isAdmin(): bool {
     return isLogged() && $_SESSION['role'] === 'admin';
 }
 
-function isPremium() {
-    return isLogged() && $_SESSION['premium_expire'] && strtotime($_SESSION['premium_expire']) > time();
+function isPremium(): bool {
+    return isLogged()
+        && !empty($_SESSION['premium_expire'])
+        && strtotime($_SESSION['premium_expire']) > time();
 }
