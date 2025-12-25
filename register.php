b3425d36d9b116440927a0faf6031a45f1b13adc
Ugryzh <34131622+ugryzh@users.noreply.github.com>
2025-12-25 19:05:31 +0100

    Create register.php

---
+++ register.php
@@ -0,0 +1,7 @@
+<form method="post" action="api/register.php">
+  <h2>Rejestracja</h2>
+  <input name="username" placeholder="Nazwa użytkownika" required>
+  <input name="email" type="email" placeholder="Email" required>
+  <input name="password" type="password" placeholder="Hasło" required>
+  <button>Zarejestruj</button>
+</form>
