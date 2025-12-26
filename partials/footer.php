59ed82ce60dfcfa8acbd974ab6745d6dc80bae25
Ugryzh <34131622+ugryzh@users.noreply.github.com>
2025-12-26 14:43:27 +0100

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -2,20 +2,39 @@
   © <?= date('Y') ?> Hangul Learn
 </footer>
 
+<!-- AUTH POPUP -->
 <div id="authOverlay" class="auth-overlay">
   <div class="auth-modal">
 
     <h3 id="authTitle">Logowanie</h3>
 
-    <form id="authForm">
-      <input name="username" placeholder="Nazwa użytkownika" required>
-      <input type="password" name="password" placeholder="Hasło" required>
-      <button id="authSubmit">Zaloguj</button>
+    <form id="authForm" autocomplete="off">
+      <input
+        type="text"
+        name="username"
+        placeholder="Nazwa użytkownika"
+        required
+      >
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Hasło"
+        required
+      >
+
+      <button type="submit" id="authSubmit">
+        Zaloguj
+      </button>
     </form>
 
-    <div id="authSwitch" class="auth-switch"></div>
+    <div id="authSwitch" class="auth-switch">
+      Nie masz konta?
+      <a href="#" id="authSwitchLink">Zarejestruj się</a>
+    </div>
 
   </div>
 </div>
 
+<!-- JS MUSI BYĆ NA SAMYM KOŃCU -->
 <script src="/assets/js/auth.js"></script>
